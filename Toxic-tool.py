8dad1789a2407e378bff4cc250ebfd575d27a5d0
walidaresto <70635262+walidaresto@users.noreply.github.com>
2022-12-31 14:29:59 +0100

    Update Toxic-tool.py

--- Toxic-tool.py
+++ Toxic-tool.py
@@ -26,7 +26,7 @@ def findAdmin():
 		sub_link = f.readline()
 		if not sub_link:
 			break
-		req_link = "http://"+link+sub_link"
+		req_link = "http://"+link+sub_link
 		req = Request(req_link)
 		try:
 			response = urlopen(req)
